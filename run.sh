3ba9b6536fa5e3a01f3e9c727a40605cd639e9bd
YHHHCF <simengliao123@instance-2.us-east1-d.c.gcpsocialnetwork.internal>
2019-12-10 01:40:42 +0000

    readme

--- run.sh
+++ run.sh
@@ -1,27 +1,11 @@
 echo Each experiment runs 5 times, the shortest/average run-time are as follows:
 
 echo First run ==========================
-echo Run Python CPU:
-python3 benchmark.py py cpu
+# echo Run Python CPU:
+# python3 benchmark.py py cpu
 
-echo Run C++ CPU:
-python3 benchmark.py cpp cpu
-
-echo Run Python GPU:
-python3 benchmark.py py gpu
-
-echo Run C++ GPU:
-python3 benchmark.py cpp gpu
-
-echo Run CUDA GPU:
-python3 benchmark.py cuda gpu
-
-echo Second run ==========================
-echo Run Python CPU:
-python3 benchmark.py py cpu
-
-echo Run C++ CPU:
-python3 benchmark.py cpp cpu
+# echo Run C++ CPU:
+# python3 benchmark.py cpp cpu
 
 echo Run Python GPU:
 python3 benchmark.py py gpu
@@ -32,18 +16,34 @@ python3 benchmark.py cpp gpu
 echo Run CUDA GPU:
 python3 benchmark.py cuda gpu
 
-echo Third run ==========================
-echo Run Python CPU:
-python3 benchmark.py py cpu
+# echo Second run ==========================
+# echo Run Python CPU:
+# python3 benchmark.py py cpu
 
-echo Run C++ CPU:
-python3 benchmark.py cpp cpu
+# echo Run C++ CPU:
+# python3 benchmark.py cpp cpu
 
-echo Run Python GPU:
-python3 benchmark.py py gpu
+# echo Run Python GPU:
+# python3 benchmark.py py gpu
 
-echo Run C++ GPU:
-python3 benchmark.py cpp gpu
+# echo Run C++ GPU:
+# python3 benchmark.py cpp gpu
 
-echo Run CUDA GPU:
-python3 benchmark.py cuda gpu
+# echo Run CUDA GPU:
+# python3 benchmark.py cuda gpu
+
+# echo Third run ==========================
+# echo Run Python CPU:
+# python3 benchmark.py py cpu
+
+# echo Run C++ CPU:
+# python3 benchmark.py cpp cpu
+
+# echo Run Python GPU:
+# python3 benchmark.py py gpu
+
+# echo Run C++ GPU:
+# python3 benchmark.py cpp gpu
+
+# echo Run CUDA GPU:
+# python3 benchmark.py cuda gpu
